b601dbb45815fd180252dc6e2149d97e3ed1c224
Kiet Pham <kietpham.dev@gmail.com>
2021-09-19 17:29:23 +1000

    Using React Portals

--- components/ui/notification.tsx
+++ components/ui/notification.tsx
@@ -16,11 +16,12 @@ function Notification(props: any) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById('notification')
   );
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <div id='notification'></div>
         </body>
       </Html>
     );
