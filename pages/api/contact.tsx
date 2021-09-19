9fb330cdb799761ad2c28457098f9d63ee7fb3c1
Kiet Pham <kietpham.dev@gmail.com>
2021-09-19 13:06:06 +1000

    storing message with mongodb database

--- pages/api/contact.tsx
+++ pages/api/contact.tsx
@@ -1,11 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { MongoClient } from 'mongodb';
+import config from './../../config/prod.json';
+
+// 2d2suVhhrAxiwmNg
+// nextblog
 type Data = {
   message: string;
-  data?: {} | null;
+  data?: any;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -25,12 +30,29 @@ export default function handler(
     }
 
     // Store it in a database
-    const newMessage = {
+    let newMessage = {
       email,
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+    try {
+      client = await MongoClient.connect(config.dbConnectionString);
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+    }
+
+    const db = client?.db();
+
+    try {
+      await db?.collection('messages').insertOne(newMessage);
+    } catch (error) {
+      client?.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+    } finally {
+      client?.close();
+    }
 
     res
       .status(201)
